bcd52e9a79312913d22ec0b2b2f35cf61e785251
David Warrington <davidmwarrington+github@gmail.com>
2022-01-03 19:10:04 +0000

    feat(eslint): prefer named exports

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
+
+  rules: {
+    'import/no-default-export': ['error'],
+    'import/prefer-default-export': ['off'],
+  },
 };
