823145a7044469bb7475175d3a2915be4647e8cb
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-03-22 23:49:20 +0000

    feat(stylelint): add types for config (#22)

---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,3 @@
+import config from './stylelint.config.js';
+
+export default config;
--- packages/stylelint-config/stylelint.config.js
+++ packages/stylelint-config/stylelint.config.js
@@ -1,3 +1,10 @@
-export default {
+// @ts-check
+
+/** @typedef {import('stylelint').Config} Config */
+
+/** @satisfies {Config} */
+const config = {
   extends: ['stylelint-config-standard'],
 };
+
+export default config;
