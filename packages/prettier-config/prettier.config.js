1d7bfaad4c7cc6c8220649d4418bfa813c7edc81
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-03-22 22:16:05 +0000

    feat(prettier): add types for config

--- packages/prettier-config/prettier.config.js
+++ packages/prettier-config/prettier.config.js
@@ -1,5 +1,12 @@
-export default {
+// @ts-check
+
+/** @typedef {import('prettier').Config} Config */
+
+/** @satisfies {Config} */
+const config = {
   arrowParens: 'avoid',
   semi: true,
   singleQuote: true,
 };
+
+export default config;
