8b4b44812bf95a17648452747de55906a8b49986
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-03-22 21:47:43 +0000

    feat(commitlint): add types for config

--- packages/commitlint-config/commitlint.config.js
+++ packages/commitlint-config/commitlint.config.js
@@ -1,3 +1,10 @@
-export default {
+// @ts-check
+
+/** @typedef {import('@commitlint/types').UserConfig} UserConfig */
+
+/** @satisfies {UserConfig} */
+const config = {
   extends: ['@commitlint/config-conventional'],
 };
+
+export default config;
