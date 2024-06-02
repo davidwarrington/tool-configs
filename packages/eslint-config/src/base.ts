19aa971b9f56237e161bb1c38e3dc858ff2616c9
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-06-02 20:10:50 +0100

    feat(eslint): allow array callback references and a few abbreviations (#51)
    * feat(eslint): allow some abbreviations
    
    * feat(eslint): disabled no array callback reference

--- packages/eslint-config/src/base.ts
+++ packages/eslint-config/src/base.ts
@@ -8,9 +8,21 @@ export default tseslint.config(
   {
     rules: {
       'unicorn/import-style': 'off',
+      'unicorn/no-array-callback-reference': 'off',
       'unicorn/no-array-for-each': 'off',
       'unicorn/no-array-reduce': 'off',
       'unicorn/prefer-switch': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          replacements: {
+            db: false,
+            env: false,
+            props: false,
+            src: false,
+          },
+        },
+      ],
     },
   },
 );
