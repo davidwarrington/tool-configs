de81fc87d2bf3147d006fb8959aacba4c5d0a2e2
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-06-03 20:31:21 +0100

    feat(stylelint): add tests for bemit selectors

---
+++ packages/stylelint-config/bemit/index.spec.ts
@@ -0,0 +1,60 @@
+import { describe, expect, it } from 'vitest';
+import { getTestRule } from 'vitest-stylelint-utils';
+import config from './stylelint.config';
+
+const testRule = getTestRule({
+  describe,
+  expect,
+  it,
+});
+
+const ruleName = 'selector-class-pattern';
+const selectors = {
+  pass: [
+    '.o-layout',
+    '.c-site-header',
+    '.c-site-header__interior',
+    '.c-site-header--fixed',
+    // eslint-disable-next-line unicorn/prefer-string-raw
+    '.c-site-header--fixed\\\\@md',
+    '.u-bg-tomato',
+    '.u-max-w-prose',
+  ],
+  fail: [
+    '.c-site-header--fixed__interior',
+    '.c-site-header--fixed--small',
+    '.c-site-header__interior__nav',
+    '.c-site-header@md__interior__nav',
+    '.c-site-header@md--fixed',
+  ],
+};
+
+testRule({
+  ruleName,
+  config: config.rules[ruleName],
+
+  accept: selectors.pass.map(selector => {
+    return { code: `${selector} {}` };
+  }),
+
+  reject: selectors.fail.map(selector => {
+    return {
+      code: `${selector} {}`,
+      message: `Expected "${selector}" to match pattern "^(o|c|u)-[a-z]+(-[a-z0-9]+)*(__[a-z0-9]+(-[a-z0-9]+)*)?(--[a-z0-9]+(-[a-z0-9]+)*)?(\\\\@[a-z]+)?$" (selector-class-pattern)`,
+    };
+  }),
+});
+
+// describe('bemit/selector-class-pattern', () => {
+//   it('selector-class-pattern', async () => {
+//     const code = `.o-layout {}`;
+//     const output = await lint({
+//       code,
+//       config,
+//     });
+
+//     expect(output.results[0].warnings).toEqual([]);
+//     expect(output.results[0].parseErrors).toEqual([]);
+//     expect(output.results[0].invalidOptionWarnings).toEqual([]);
+//   });
+// });
--- packages/stylelint-config/bemit/stylelint.config.js
+++ packages/stylelint-config/bemit/stylelint.config.js
@@ -5,24 +5,6 @@
 /** @satisfies {Config} */
 const config = {
   rules: {
-    /**
-     * Match BEMIT
-     *
-     * Test cases:
-     * o-layout => Pass
-     * c-site-header => Pass
-     * c-site-header__interior => Pass
-     * c-site-header--fixed => Pass
-     * c-site-header--fixed\\@md => Pass
-     * u-bg-tomato => Pass
-     * u-max-w-prose => Pass
-     *
-     * c-site-header--fixed__interior => Fail
-     * c-site-header--fixed--small => Fail
-     * c-site-header__interior__nav => Fail
-     * c-site-header\\@md__interior__nav => Fail
-     * c-site-header\\@md--fixed => Fail
-     */
     'selector-class-pattern':
       // eslint-disable-next-line unicorn/prefer-string-raw
       '^(o|c|u)-[a-z]+(-[a-z0-9]+)*(__[a-z0-9]+(-[a-z0-9]+)*)?(--[a-z0-9]+(-[a-z0-9]+)*)?(\\\\@[a-z]+)?$',
