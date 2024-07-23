a1cbd6c5b92091bb889047f607dd51dc2537131d
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-07-23 21:15:01 +0100

    feat(prettier): disable formatting on pnpm-lock

--- packages/prettier-config/prettier.config.js
+++ packages/prettier-config/prettier.config.js
@@ -1,12 +1,28 @@
 // @ts-check
 
 /** @typedef {import('prettier').Config} Config */
+/** @typedef {NonNullable<Config['overrides']>[number]} ConfigOverride */
 
 /** @satisfies {Config} */
 const config = {
   arrowParens: 'avoid',
   semi: true,
   singleQuote: true,
+
+  overrides: [requireOptIn(['pnpm-lock.yaml'])],
 };
 
 export default config;
+
+/** @param {ConfigOverride['files']} files */
+function requireOptIn(files) {
+  /** @satisfies {ConfigOverride} */
+  const override = {
+    files,
+    options: {
+      requirePragma: true,
+    },
+  };
+
+  return override;
+}
