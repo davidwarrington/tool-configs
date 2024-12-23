1b8fcf20a307572df44728f404572d1cf5a8952e
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-12-23 22:24:25 +0000

    fix: use named eslint config import

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 
-import configs from '@davidwarrington/eslint-config';
+import { typescript } from '@davidwarrington/eslint-config';
 import { config } from 'typescript-eslint';
 
-export default config(...configs.typescript, {
+export default config(...typescript, {
   ignores: ['**/dist'],
 });
