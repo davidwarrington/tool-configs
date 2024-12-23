b8ea015adfa660cb9382c263091fa5dcf3a27ed3
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2024-12-23 22:29:19 +0000

    feat(stylelint): allow private prefix for custom properties (#79)

--- packages/stylelint-config/stylelint.config.js
+++ packages/stylelint-config/stylelint.config.js
@@ -2,11 +2,22 @@
 
 /** @typedef {import('stylelint').Config} Config */
 
+const privateCustomPropertyPrefix = String.raw`\\#`;
+const customPropertyPattern = `^(${privateCustomPropertyPrefix})?([a-z][a-z0-9]*)(-[a-z0-9]+)*$`;
+
 /** @satisfies {Config} */
 const config = {
   extends: ['stylelint-config-standard'],
 
   rules: {
+    'custom-property-pattern': [
+      customPropertyPattern,
+      {
+        /** @param {string} name */
+        message: name =>
+          `Expected custom media query name "${name}" to be kebab-case`,
+      },
+    ],
     'declaration-block-no-redundant-longhand-properties': [
       true,
       {
