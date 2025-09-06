0b76c888fe7d50737a331a99cfcbfd522d5003ab
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2025-09-06 19:14:43 +0100

    feat(eslint): add import plugin

--- packages/eslint-config/src/typescript.ts
+++ packages/eslint-config/src/typescript.ts
@@ -1,5 +1,29 @@
-import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
 import { defineConfig } from 'eslint/config';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import { importX } from 'eslint-plugin-import-x';
+import tseslint from 'typescript-eslint';
 import base from './base';
 
-export default defineConfig(...base, ...tseslint.configs.recommended);
+export default defineConfig(
+  base,
+  tseslint.configs.recommended,
+  // @ts-expect-error outdated types
+  importX.flatConfigs.typescript,
+
+  {
+    name: 'import-x-settings',
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parser: tsParser,
+      sourceType: 'module',
+    },
+    settings: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
+          project: ['tsconfig.json'],
+        }),
+      ],
+    },
+  },
+);
