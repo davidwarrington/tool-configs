40f872c6cad03c10593099d41347546f840f4b8d
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2026-01-01 16:54:42 +0000

    feat(eslint): export prevent abbreviations config

---
+++ packages/eslint-config/src/rule-configs/prevent-abbreviations.ts
@@ -0,0 +1,12 @@
+export const preventAbbreviationsConfig = {
+  replacements: {
+    args: false,
+    db: false,
+    docs: false,
+    dist: false,
+    env: false,
+    params: false,
+    props: false,
+    src: false,
+  },
+};
